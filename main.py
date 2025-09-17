b5c5f1361e6b9d00a8216e07d7112f45b251421c
Nicholas Kitchen <njkitchen@mail.fhsu.edu>
2025-09-17 14:11:07 -0500

    Added code to save graphs to charts. Cleaned up code.

--- main.py
+++ main.py
@@ -2,14 +2,21 @@
 # Nicholas Kitchen
 # Mini Project 2
 
-# My question: Which country has had the highest increase in GDP from 2020 and 2025? What were the top 5?
+# My question: Which country had the highest increase in GDP from 2020 to 2025? What were the top 5?
 # Note: If a country does not have data available for 2020 or 2025, they will be excluded.
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
+
 # Read in csv dataset
 df = pd.read_csv("./data/2020-2025.csv")
+
+# Create charts folder if it does not exist
+charts = Path('charts')
+if not charts.exists():
+    Path(r'charts').mkdir()
 
 # Remove any country that does not have data for the years 2020 and 2025
 df = df.dropna().reset_index(drop=True)
@@ -17,30 +24,35 @@ df = df.dropna().reset_index(drop=True)
 # Create a new column "delta" to find the change between GDP in 2020 and 2025
 df["delta"] = df["2025"] - df["2020"]
 
-# Get the row of the country with the highest GDP increase
+# Get the row of the country with the highest GDP increase along with the top 5
 highest_increase_country = df.loc[df["delta"].idxmax()]
-top5_highest_increase_countries = df.nlargest(5, "delta")
+top5_countries = df.nlargest(5, "delta")
 
 # Create a graph that shows the GDP of each year for the top 5 countries
 years = ["2020", "2021", "2022", "2023", "2024", "2025"]
 
 fig, ax = plt.subplots(figsize=(8,5))
 
-for _, row in top5_highest_increase_countries.iterrows():
+for _, row in top5_countries.iterrows():
     ax.plot(years, row[years] / 1_000_000, marker="o", label=row["Country"])
 
-ax.set_ylim(0, top5_highest_increase_countries[years].max().max() / 1_000_000 * 1.1)
+ax.yaxis.grid(True, linestyle='--', linewidth=0.7, alpha=0.7)
+
 ax.set_ylabel("GDP (in millions)")
 ax.set_xlabel("Year")
-ax.set_title("Top 5 Countries - GDP by Year")
+ax.set_title("Top 5 Highest Increase Countries - GDP by Year")
 ax.legend(title="Country", bbox_to_anchor=(1.05, 1), loc="upper left")
 
+ax.set_ylim(0, 33)
+ax.set_yticks(np.arange(0, 33, 2.5))
+
 plt.tight_layout()
-plt.show()
+plt.savefig(f'charts/Top_5_Countries_-_GDP_by_Year.png')
+print(f'Saving Top_5_Countries_-_GDP_by_Year.png to charts folder.')
 
 # Create a graph that shows the GDP growth of the top 5 countries from 2020 to 2025
 fig, ax = plt.subplots(figsize=(6,4))
-ax.bar(top5_highest_increase_countries["Country"], top5_highest_increase_countries["delta"] / 1_000_000)
+ax.bar(top5_countries["Country"], top5_countries["delta"] / 1_000_000)
 
 ax.set_ylabel("Delta (in millions)")
 ax.set_xlabel("Country")
@@ -51,13 +63,15 @@ ax.set_ylim(0, 10)
 ax.set_yticks(np.arange(0, 11, 1))
 
 plt.tight_layout()
-plt.show()
+plt.savefig(f'charts/GDP_Growth_from_2020_to_2025.png')
+print(f'Saving GDP_Growth_from_2020_to_2025.png to charts folder.')
 
+print()
 print("The country with the highest GDP growth between 2020 and 2025 is:")
 print(highest_increase_country[["Country", "2020","2025", "delta"]])
 print()
 print("The top 5 countries with the highest GDP growth between 2020 and 2025 are:")
-print(top5_highest_increase_countries[["Country", "2020","2025", "delta"]])
+print(top5_countries[["Country", "2020","2025", "delta"]])
 
 # Please submit a link to your GitHub project. Do not submit your project files here!
 #
